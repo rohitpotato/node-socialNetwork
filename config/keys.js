0e539a8d7a4a26e8afbfccfb3caa819e7b25e76d
Rohit Kashyap <rohit.mmm1996@gmail.com>
2018-06-05 05:17:55 +0530

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,7 @@
 module.exports = {
 
-	googleClientID: '958721642848-3s7i4qepm4kg3m0u8fqjvua18m1tjfd9.apps.googleusercontent.com',
-	googleClientSecret: 'IClRuO5hqM_M8rP0BGq38YZu',
-	cookieKey: 'abcdefghijklmnopqrstuvw12312'
+	googleClientID: 'your-google-clienID',
+	googleClientSecret: 'your-google-secret',
+	cookieKey: 'your-cookie-key'
 
 };
